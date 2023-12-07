d2f65aac0fbf9758b7eeac3ea93c8c9d024593b4
Antonio Valdivia <JoseAntonio.Ramirez@Solera.com>
2023-12-07 16:37:19 -0600

    First commit

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+// Variables y constantes
+
+const nombre = 'Antonio';
+const apellido = 'Valdivia';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var no se debe usar...
+
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre);
+}   
+
+console.log(valorDado);
---
+++ src/bases/02-template-string.js
@@ -0,0 +1,16 @@
+// Template string
+
+const nombre = 'Antonio';
+const apellido = 'Valdvia';
+
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+let getSaludo = (nombre) => {
+    return `Hola ${nombre}`;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)}`);
