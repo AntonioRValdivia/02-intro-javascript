d9dbd8c889d76571b09d546d47ebd62bbd144197
Antonio Valdivia <JoseAntonio.Ramirez@Solera.com>
2024-02-05 18:00:17 -0600

    imp-exp

---
+++ src/bases/data/heroes.js
@@ -0,0 +1,37 @@
+ export const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];
+
+// export default heroes; //    Exportacion por defecto
+// export const owners = [DC, Marvel];     // Se puede exportar por defecto y por separado
+
+// En un solo array por defecto y por separado
+// export {
+//     heroes as default,
+//     owners
+// }
+
